0d76667e091abe73e46bf6e5bea4ebc7e2453813
dipakahirav <dipaksahirav@gmail.com>
2026-02-11 15:02:54 +0530

    signup controller added

---
+++ backend/src/lib/utils.js
@@ -0,0 +1,21 @@
+import jwt from "jsonwebtoken";
+
+export const generateToken = (userId, res) => {
+  const { JWT_SECRET } = process.env;
+  if (!JWT_SECRET) {
+    throw new Error("JWT_SECRET is not configured");
+  }
+
+  const token = jwt.sign({ userId }, JWT_SECRET, {
+    expiresIn: "7d",
+  });
+
+  res.cookie("jwt", token, {
+    maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days in milliseconds
+    httpOnly: true, // prevent XSS attacks: cross-site scripting attacks
+    sameSite: "strict", // prevent CSRF attacks
+    secure: process.env.NODE_ENV === "development" ? false : true, // prevent MITM attacks: man-in-the-middle attacks
+  });
+
+  return token;
+};
