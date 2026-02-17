8bac0f4d836ac266ca1bd409b718fe2788224f43
dipakahirav <dipaksahirav@gmail.com>
2026-02-17 11:54:13 +0530

    online users implemented

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,18 +1,19 @@
 import express from "express";
+import cookieParser from "cookie-parser";
 import path from "path";
+import cors from "cors";
+
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 import { connectDB } from "./lib/db.js";
 import { ENV } from "./lib/env.js";
-import cookieParser from "cookie-parser";
-import cors from "cors";
+import { app, server } from "./lib/socket.js";
 
-const app = express();
 const __dirname = path.resolve();
 
 const PORT = ENV.PORT || 3000;
 
-app.use(express.json({ limit: "5mb" })); // to parse the incoming requests with JSON payloads (from req.body)
+app.use(express.json({ limit: "5mb" })); // req.body
 app.use(cors({ origin: ENV.CLIENT_URL, credentials: true }));
 app.use(cookieParser());
 
@@ -22,11 +23,13 @@ app.use("/api/messages", messageRoutes);
 // make ready for deployment
 if (ENV.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "../frontend", "dist", "index.html"));
+
+  app.get("*", (_, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
   });
 }
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+
+server.listen(PORT, () => {
+  console.log("Server running on port: " + PORT);
   connectDB();
 });
