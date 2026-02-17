46cbe88d330f0c6eecea92f1bdc2f0000b8c3862
dipakahirav <dipaksahirav@gmail.com>
2026-02-17 21:58:12 +0530

    axios url changes

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export const axiosInstance = axios.create({
   baseURL:
     import.meta.env.MODE === "development"
-      ? "http://localhost:3000/api"
-      : "/api",
+      ? `${import.meta.env.VITE_API_URL}/api`
+      : `${import.meta.env.VITE_API_URL}/api`,
   withCredentials: true,
 });
